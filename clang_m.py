49ed7c3f0924f56f508b95d741a6163c9ac7a21d
wolf1996 <ksgiv37@gmail.com>
2016-07-13 13:35:30 +0300

    added clangs steps config command, path provlem solution

--- clang_m.py
+++ clang_m.py
@@ -2,16 +2,17 @@
     clang interface
 """
 
+import copy
 import datetime
 import subprocess
 import logging
-import os
 import os.path as opath
 import glob
 import plistlib
 import interface
 
-#заменить всё на темповые файлы и директории
+# заменить всё на темповые файлы и директории
+
 
 class Clang(interface.Interface):
     """
@@ -20,7 +21,7 @@ class Clang(interface.Interface):
 
     def __init__(self, conf):
         """
-            init of clang py interface
+            build init of clang py interface
         """
         interface.Interface.__init__(self, conf)
         self.command = "scan-build"
@@ -33,11 +34,9 @@ class Clang(interface.Interface):
             load from file some data in xml format and
             parse it
         """
-        print(path)
         cur = glob.glob("{}/**/*.plist".format(path))
         res = []
         for i in cur:
-            print(i)
             with open(i, "rb") as plistfile:
                 obj = plistlib.load(plistfile)
                 if obj["diagnostics"]:
@@ -46,16 +45,118 @@ class Clang(interface.Interface):
 
     @staticmethod
     def __get_dir_name__():
+        """
+            get directory name
+        """
         timevar = datetime.datetime.now()
         dirname = timevar.strftime("%d%m%Y_%H%M%S")
         dirname = opath.abspath("./{}".format(dirname))
         return dirname
 
+# комманды добавляюще анализируемую комманду к команде анализатора
+# из-за проблем с относительными путями
 
+    def __clean_command(self, cmd, path):
+        """
+            clean command
+        """
+        conf = self.get_conf()
+        if "cleancommand" in conf:
+            cmd = copy.copy(cmd)
+            com = conf["cleancommand"].split()
+            bufcom = opath.join(path, com[0])
+            if opath.isfile(bufcom):
+                com[0] = bufcom
+            cmd += com
+            cmd_str = ""
+            for i in cmd:
+                cmd_str += i
+                cmd_str += " "
+            self.logger.debug("scan-build clean command: " + cmd_str)
+            outfile = open("out_clean_clang", "w+")
+            errfile = open("err_clean_clang", "w+")
+            print(path)
+            print(cmd)
+            proc = subprocess.Popen(
+                cmd, cwd=path, stdout=outfile, stderr=errfile)
+            proc.wait()
+            retcode = proc.returncode
+            for i in outfile:
+                self.logger.info(i)
+            for i in errfile:
+                self.logger.error(i)
+            self.logger.info("clean return code is %d", retcode)
+            outfile.close()
+            errfile.close()
 
-#разбить функцию 
-#привести всё к адекватному виду
-#добавить clean
+    def __config_command(self, cmd, path):
+        """
+            config command
+        """
+        conf = self.get_conf()
+        if "configcommand" in conf:
+            cmd = copy.copy(cmd)
+            com = conf["configcommand"].split()
+            bufcom = opath.join(path, com[0])
+            if opath.isfile(bufcom):
+                com[0] = bufcom
+            cmd += com
+            cmd_str = ""
+            for i in cmd:
+                cmd_str += i
+                cmd_str += " "
+            self.logger.debug("scan-build config command: " + cmd_str)
+            outfile = open("out_conf_clang", "w+")
+            errfile = open("err_conf_clang", "w+")
+            print(path)
+            print(cmd)
+            proc = subprocess.Popen(
+                cmd, cwd=path, stdout=outfile, stderr=errfile)
+            proc.wait()
+            retcode = proc.returncode
+            for i in outfile:
+                self.logger.info(i)
+            for i in errfile:
+                self.logger.error(i)
+            self.logger.info("clean return code is %d", retcode)
+            outfile.close()
+            errfile.close()
+
+    def __make_command(self, cmd, path):
+        """
+            make command
+        """
+        cmd = copy.copy(cmd)
+        conf = self.get_conf()
+        if "makecommand" in conf:
+            com = conf["makecommand"].split()
+            bufcom = opath.join(path, com[0])
+            if opath.isfile(bufcom):
+                com[0] = bufcom
+            cmd += com
+        else:
+            cmd.append("make")
+        cmd_str = ""
+        for i in cmd:
+            cmd_str += i
+            cmd_str += " "
+        self.logger.debug("scan-build make command: " + cmd_str)
+        outfile = open("out_make_clang", "w+")
+        errfile = open("err_make_clang", "w+")
+        print(path)
+        print(cmd)
+        proc = subprocess.Popen(
+            cmd, cwd=path, stdout=outfile, stderr=errfile)
+        proc.wait()
+        retcode = proc.returncode
+        for i in outfile:
+            self.logger.info(i)
+        for i in errfile:
+            self.logger.error(i)
+        self.logger.info("clean return code is %d", retcode)
+        outfile.close()
+        errfile.close()
+
     def get_res(self, path):
         """
             return result of analysis code from 'path'
@@ -63,12 +164,12 @@ class Clang(interface.Interface):
         path = opath.abspath(path)
         interface.Interface.get_res(self, path)
         conf = self.get_conf()
-        # формирование комманды
+        # формирование комманды (всё относящееся к анализатору)
         cmd = [self.command, ]
         if "flags" in conf:
             cmd += self.get_conf()["flags"].split(',')
         for i in conf:
-            if i not in ["flags", "makecommand", "configcommand"]:
+            if i not in ["flags", "makecommand", "configcommand", "cleancommand"]:
                 if len(i) == 1:
                     cmd.append("-{}".format(i))
                     cmd.append(conf[i])
@@ -79,53 +180,9 @@ class Clang(interface.Interface):
         cmd.append("-o")
         dirname = Clang.__get_dir_name__()
         cmd.append(dirname)
-        if "configcommand" in conf:
-            confcmd = cmd.copy()
-            confcmd.append(path+'/'+conf["configcommand"])
-            cmd_str = ""
-            for i in confcmd:
-                cmd_str += i
-                cmd_str += " "
-            outfile = open("outc_clang", "w+")
-            errfile = open("errc_clang", "w+")
-            self.logger.debug("scan-build config command: " + cmd_str)
-            print(path)
-            print(confcmd)
-            proc = subprocess.Popen(confcmd, cwd=path, stdout=outfile, stderr=errfile)
-            proc.wait()
-            retcode = proc.returncode
-            for i in outfile:
-                self.logger.info(i)
-            for i in errfile:
-                self.logger.error(i)
-            self.logger.info("config return code is %d", retcode)
-            outfile.close()
-            errfile.close()
-        if "makecommand" in conf:
-            cmd += conf["makecommand"].split()
-        else:
-            cmd.append("make")
-        # формирование строкового эквивалента комманды
-        cmd_str = ""
-        for i in cmd:
-            cmd_str += i
-            cmd_str += " "
-        self.logger.debug("scan-build command: " + cmd_str)
-        # запуск процесса
-        outfile = open("out_clang", "w+")
-        errfile = open("err_clang", "w+")
-        print(path)
-        print(cmd)
-        proc = subprocess.Popen(cmd, cwd=path, stdout=outfile, stderr=errfile)
-        proc.wait()
-        retcode = proc.returncode
-        for i in outfile:
-            self.logger.info(i)
-        for i in errfile:
-            self.logger.error(i)
-        self.logger.info("return code is %d", retcode)
-        outfile.close()
-        errfile.close()
+        self.__config_command(cmd, path)
+        self.__make_command(cmd, path)
+        self.__clean_command(cmd, path)
         #buf_str = ""
         # запись вывода в log
         err_arr = Clang.__load_report__(dirname)
