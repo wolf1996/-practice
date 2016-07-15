bda2970fd336245bdd7625aab0f960160ec53551
wolf1996 <ksgiv37@gmail.com>
2016-07-16 01:14:06 +0300

    interface added

---
+++ agregator.py
@@ -0,0 +1,23 @@
+import module
+import argparse
+import os.path as opath
+
+def main():
+    app = module.App()
+    parser = argparse.ArgumentParser(description="Some description")
+    parser.add_argument("path", type=module.exist, help="path to codebase")
+    parser.add_argument("--config", help="path to config file")
+    parsed = parser.parse_args()
+    if parsed.config:
+        app.load_config_from_file(parsed.config)
+    res = app.get_res(parsed.path)
+    for i in res:
+        buf = module.App.str_elem(i)
+        print(buf)
+        print("#######################################")
+
+
+
+
+if __name__ == '__main__':
+    main()
