c6f8a954629fa261fdf26729cc13a55554a57362
wolf1996 <ksgiv37@gmail.com>
2016-08-25 13:56:52 +0300

    add report

--- agregator.py
+++ agregator.py
@@ -7,9 +7,12 @@ def main():
     parser = argparse.ArgumentParser(description="Some description")
     parser.add_argument("path", type=module.exist, help="path to codebase")
     parser.add_argument("--config", help="path to config file")
+    parser.add_argument("--workdir", help="path to config file")
     parsed = parser.parse_args()
     if parsed.config:
         app.load_config_from_file(parsed.config)
+    if parsed.workdir:
+        app.set_workdir(parsed.workdir)
     res = app.get_res(parsed.path)
     for i in res:
         buf = module.App.str_elem(i)
